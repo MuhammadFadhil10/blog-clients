bf9e5804d4faf15ee0dfb620a71599597cb7020a
muhammadfadhil10 <muhammadfadhilakbar10@gmail.com>
2022-08-30 20:44:46 +0700

    create first navigation component

---
+++ src/components/Navigation.js
@@ -0,0 +1,12 @@
+import {} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Navigation = () => {
+	return (
+		<>
+			<h1>hello from navigation</h1>
+		</>
+	);
+};
+
+export default Navigation;
