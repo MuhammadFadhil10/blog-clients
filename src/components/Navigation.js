fb6aadfdbd297e888a7291d68f52d1ce0ec286f0
muhammadfadhil10 <muhammadfadhilakbar10@gmail.com>
2022-08-30 20:57:29 +0700

    finishing basic navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,10 +1,28 @@
-import {} from 'react-bootstrap';
+import { Navbar, Container } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Link } from 'react-router-dom';
 
 const Navigation = () => {
 	return (
 		<>
-			<h1>hello from navigation</h1>
+			<Navbar bg='dark' variant='dark'>
+				<Container>
+					<Navbar.Brand href='#home'>
+						<img
+							alt=''
+							src=''
+							width='30'
+							height='30'
+							className='d-inline-block align-top'
+						/>{' '}
+						Read My Blog
+					</Navbar.Brand>
+				</Container>
+				<Container className='d-flex justify-content-evenly'>
+					<Link to='/login'>Login</Link>
+					<Link to='/register'>Register</Link>
+				</Container>
+			</Navbar>
 		</>
 	);
 };
