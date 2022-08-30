7f52072996115f82278b9ee5bdc75ec647e2eb67
muhammadfadhil10 <muhammadfadhilakbar10@gmail.com>
2022-08-30 18:36:14 +0700

    create-react-app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+	document.title = 'Read my blog!';
+	return (
+		<div className='App'>
+			<h1>hello from react</h1>
+		</div>
+	);
+}
+
+export default App;
