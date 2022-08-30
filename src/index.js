41e4b80dfd8adb542402efa81187db9fd733f77c
Muhammad Fadhil Akbar <82107511+MuhammadFadhil10@users.noreply.github.com>
2022-08-30 20:37:54 +0700

    React router dom (#1)
    * create navigation component
    
    * installing bootstrap
    
    * import bootstrap
    
    * setup react-router-dom
    
    * Delete Navigation.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter, Routes, Route, Router } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
+	<BrowserRouter>
+		<Routes>
+			<Route path='/' element={<App />}></Route>
+		</Routes>
+	</BrowserRouter>
 );
