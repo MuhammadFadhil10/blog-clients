103981d2cce98d9d7d937687b22e446aed5f980f
muhammadfadhil10 <muhammadfadhilakbar10@gmail.com>
2022-08-31 19:35:10 +0700

    create blog list component

---
+++ src/components/BlogList.js
@@ -0,0 +1,14 @@
+import { Card, Button, Image , Container } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';\
+
+const BlogList = () => {
+    return ( 
+        <>
+            <Container>
+                <Image src='#' ></Image>
+            </Container>
+        </>
+     );
+}
+ 
+export default BlogList;
