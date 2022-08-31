f642305527a227659d0af3483e4d8ef7e878c487
muhammadfadhil10 <muhammadfadhilakbar10@gmail.com>
2022-08-31 19:41:38 +0700

    create style for blog list

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -1,14 +1,15 @@
-import { Card, Button, Image , Container } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';\
+import { Card, Button, Image, Container } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Class from './BlogList.module.css';
 
 const BlogList = () => {
-    return ( 
-        <>
-            <Container>
-                <Image src='#' ></Image>
-            </Container>
-        </>
-     );
-}
- 
+	return (
+		<>
+			<Container>
+				<Image className={`${Class.blogCover}`} src='#'></Image>
+			</Container>
+		</>
+	);
+};
+
 export default BlogList;
