18c3cd94a5c9c7b9f062c95e75f974c7870d4c71
muhammadfadhil10 <muhammadfadhilakbar10@gmail.com>
2022-08-31 19:28:06 +0700

    create a homepage content container structure and style

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
-import './App.css';
-import { Navbar, Container } from 'react-bootstrap';
+import Class from './App.module.css';
+import { Container, Image } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
 	document.title = 'Read my blog!';
 	return (
-		<div className='App'>
-			<h1>hello from react</h1>
-		</div>
+		<>
+			<Container className='d-flex flex-column justify-content-center'>
+				<div className='banner d-flex justify-content-center'>
+					<Image
+						src='#'
+						style={{
+							backgroundColor: 'salmon',
+							width: '100vw',
+							height: '12rem',
+						}}
+					></Image>
+				</div>
+				<main
+					className={`${Class.mainContainer} d-flex flex-column align-items-start `}
+				>
+					<section className={`${Class.section}  `}>
+						<h1>Your favourite topics</h1>
+						<div className={`${Class.sectionContentContainer} `}></div>
+					</section>
+					<section className={`${Class.section}  `}>
+						<h1>Your favourite topics</h1>
+						<div className={`${Class.sectionContentContainer} `}></div>
+					</section>
+					<section className={`${Class.section}  `}>
+						<h1>Your favourite topics</h1>
+						<div className={`${Class.sectionContentContainer} `}></div>
+					</section>
+				</main>
+			</Container>
+		</>
 	);
 }
 
