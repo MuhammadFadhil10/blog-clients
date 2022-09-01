76620ec67114c5020f829b1d34d1bbaf18f2d71a
muhammadfadhil10 <muhammadfadhilakbar10@gmail.com>
2022-09-01 19:39:38 +0700

    add icon for subnav

--- src/components/SubNav.js
+++ src/components/SubNav.js
@@ -1,17 +1,21 @@
 import { Navbar, Container, Image } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { CgHome, CgSearch, CgAdd, CgFolder, CgProfile } from 'react-icons/cg';
+
 import Style from './SubNav.module.css';
 
 const SubNav = () => {
 	return (
 		<>
 			<Navbar variant='dark' bg='dark' className={`${Style.navbar}`}>
-				<Container>
-					<Image src='#' className='' alt=''></Image>
-					<Image src='#' className='' alt=''></Image>
-					<Image src='#' className='' alt=''></Image>
-					<Image src='#' className='' alt=''></Image>
-					<Image src='#' className='' alt=''></Image>
+				<Container
+					className={`${Style.subnavContainer} d-flex justify-content-between`}
+				>
+					<CgHome color='white' size={30} />
+					<CgSearch color='white' size={30} />
+					<CgAdd color='white' size={30} />
+					<CgFolder color='white' size={30} />
+					<CgProfile color='white' size={30} />
 				</Container>
 			</Navbar>
 		</>
