e44280348e43cc4405f5bf2db7d0541795239b94
muhammadfadhil10 <muhammadfadhilakbar10@gmail.com>
2022-09-01 09:28:13 +0700

    create reagister form

--- src/pages/auth/Register.js
+++ src/pages/auth/Register.js
@@ -1,12 +1,39 @@
-import { Container, Form } from 'react-bootstrap';
+import { Button, Container, Form } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Style from './Register.module.css';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
 	return (
 		<>
 			<Container>
 				<h1>Create account</h1>
+				<Form>
+					<Form.Group>
+						<Form.Label>Email</Form.Label>
+						<Form.Control type='email' autoComplete='off'></Form.Control>
+					</Form.Group>
+					<Form.Group>
+						<Form.Label>Username</Form.Label>
+						<Form.Control type='text' autoComplete='off'></Form.Control>
+					</Form.Group>
+					<Form.Group>
+						<Form.Label>Name</Form.Label>
+						<Form.Control type='text' autoComplete='off'></Form.Control>
+					</Form.Group>
+					<Form.Group>
+						<Form.Label>Password</Form.Label>
+						<Form.Control type='password' autoComplete='off'></Form.Control>
+					</Form.Group>
+					<Form.Group>
+						<Form.Label>Confirm Password</Form.Label>
+						<Form.Control type='password' autoComplete='off'></Form.Control>
+					</Form.Group>
+					<Button type='button'>Register</Button>
+					<p>
+						Already have account? <Link to='/login'>Login</Link>
+					</p>
+				</Form>
 			</Container>
 		</>
 	);
