7e8ef06c1b4ee0b6f4a9834a98b8a772daf24620
muhammadfadhil10 <muhammadfadhilakbar10@gmail.com>
2022-09-01 15:07:38 +0700

    create reusable form

---
+++ src/components/Form.js
@@ -0,0 +1,64 @@
+import { Button, Container, Form } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Style from './Form.module.css';
+import { Link } from 'react-router-dom';
+
+const Form = ({ type }) => {
+	return (
+		<>
+			<Container>
+				<h1>Create account</h1>
+				<Form className={`${Style.form} d-flex flex-column align-items-start`}>
+					<Form.Group className={`${Style.formGroup} `}>
+						<Form.Label>Email</Form.Label>
+						<Form.Control
+							className={`${Style.formInput} `}
+							type='email'
+							autoComplete='off'
+						></Form.Control>
+					</Form.Group>
+					<Form.Group className={`${Style.formGroup} `}>
+						<Form.Label>Username</Form.Label>
+						<Form.Control
+							className={`${Style.formInput} `}
+							type='text'
+							autoComplete='off'
+						></Form.Control>
+					</Form.Group>
+					<Form.Group className={`${Style.formGroup} `}>
+						<Form.Label>Name</Form.Label>
+						<Form.Control
+							className={`${Style.formInput} `}
+							type='text'
+							autoComplete='off'
+						></Form.Control>
+					</Form.Group>
+					<Form.Group className={`${Style.formGroup} `}>
+						<Form.Label>Password</Form.Label>
+						<Form.Control
+							className={`${Style.formInput} `}
+							type='password'
+							autoComplete='off'
+						></Form.Control>
+					</Form.Group>
+					<Form.Group className={`${Style.formGroup} `}>
+						<Form.Label>Confirm Password</Form.Label>
+						<Form.Control
+							className={`${Style.formInput} `}
+							type='password'
+							autoComplete='off'
+						></Form.Control>
+					</Form.Group>
+					<Button type='button' variant='dark'>
+						Register
+					</Button>
+					<p>
+						Already have account? <Link to='/login'>Login</Link>
+					</p>
+				</Form>
+			</Container>
+		</>
+	);
+};
+
+export default Form;
