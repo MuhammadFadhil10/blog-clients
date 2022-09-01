6f2581217c430324160a44923144fda4cd5c2b10
muhammadfadhil10 <muhammadfadhilakbar10@gmail.com>
2022-09-01 19:17:33 +0700

    create navback component

---
+++ src/components/SubNav.js
@@ -0,0 +1,20 @@
+import { Navbar, Container, Image } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const SubNav = () => {
+	return (
+		<>
+			<Navbar variant='dark' bg='dark'>
+				<Container>
+					<Image src='#' className='' alt=''></Image>
+					<Image src='#' className='' alt=''></Image>
+					<Image src='#' className='' alt=''></Image>
+					<Image src='#' className='' alt=''></Image>
+					<Image src='#' className='' alt=''></Image>
+				</Container>
+			</Navbar>
+		</>
+	);
+};
+
+export default SubNav;
