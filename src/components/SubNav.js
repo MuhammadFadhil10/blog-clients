53700b6df4170e7b42353519236686762aa7d3c2
muhammadfadhil10 <muhammadfadhilakbar10@gmail.com>
2022-09-01 19:30:37 +0700

    create style for subnav and import to react-router

--- src/components/SubNav.js
+++ src/components/SubNav.js
@@ -1,10 +1,11 @@
 import { Navbar, Container, Image } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Style from './SubNav.module.css';
 
 const SubNav = () => {
 	return (
 		<>
-			<Navbar variant='dark' bg='dark'>
+			<Navbar variant='dark' bg='dark' className={`${Style.navbar}`}>
 				<Container>
 					<Image src='#' className='' alt=''></Image>
 					<Image src='#' className='' alt=''></Image>
--- src/index.js
+++ src/index.js
@@ -7,12 +7,14 @@ import App from './App';
 import Navigation from './components/Navigation';
 import NotFound from './pages/error/NotFound';
 import NavBack from './components/NavBack';
+import SubNav from './components/SubNav';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<BrowserRouter>
 		<Navigation />
 		<NavBack />
+		<SubNav />
 		<Routes>
 			<Route path='/' element={<App />}></Route>
 			<Route path='*' element={<NotFound />}></Route>
