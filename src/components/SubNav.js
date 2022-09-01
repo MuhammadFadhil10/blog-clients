3f39c8e70a0accfffc9d3c663c875cb59cfab54b
muhammadfadhil10 <muhammadfadhilakbar10@gmail.com>
2022-09-01 20:21:06 +0700

    create subnav pointer

--- src/components/SubNav.js
+++ src/components/SubNav.js
@@ -3,19 +3,38 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { CgHome, CgSearch, CgAdd, CgFolder, CgProfile } from 'react-icons/cg';
 
 import Style from './SubNav.module.css';
+import NavPointer from './SubNavPointer';
 
 const SubNav = () => {
 	return (
 		<>
-			<Navbar variant='dark' bg='dark' className={`${Style.navbar}`}>
+			<Navbar
+				variant='dark'
+				bg='dark'
+				className={`${Style.navbar} d-flex align-items-start `}
+			>
+				{/* <NavPointer /> */}
 				<Container
-					className={`${Style.subnavContainer} d-flex justify-content-between`}
+					className={`${Style.subnavContainer} d-flex justify-content-center `}
 				>
-					<CgHome color='white' size={30} />
-					<CgSearch color='white' size={30} />
-					<CgAdd color='white' size={30} />
-					<CgFolder color='white' size={30} />
-					<CgProfile color='white' size={30} />
+					<span
+						className={`${Style.iconContainer}`}
+						style={{ borderTop: '3px solid #9b9bee' }}
+					>
+						<CgHome color='white' size={35} />
+					</span>
+					<span className={`${Style.iconContainer}`}>
+						<CgSearch color='white' size={35} />
+					</span>
+					<span className={`${Style.iconContainer}`}>
+						<CgAdd color='white' size={35} />
+					</span>
+					<span className={`${Style.iconContainer}`}>
+						<CgFolder color='white' size={35} />
+					</span>
+					<span className={`${Style.iconContainer}`}>
+						<CgProfile color='white' size={35} />
+					</span>
 				</Container>
 			</Navbar>
 		</>
---
+++ src/components/SubNavPointer.js
@@ -0,0 +1,13 @@
+const NavPointer = () => {
+	return (
+		<div
+			style={{
+				width: '90px',
+				height: '5px',
+				backgroundColor: 'blue',
+			}}
+		></div>
+	);
+};
+
+export default NavPointer;
