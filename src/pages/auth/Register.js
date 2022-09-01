d00ad6b5135b4f3616b61ef7325d0542279e08c5
muhammadfadhil10 <muhammadfadhilakbar10@gmail.com>
2022-09-01 09:23:25 +0700

    create reagister component

---
+++ src/pages/auth/Register.js
@@ -0,0 +1,15 @@
+import { Container, Form } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Style from './Register.module.css';
+
+const Register = () => {
+	return (
+		<>
+			<Container>
+				<h1>Create account</h1>
+			</Container>
+		</>
+	);
+};
+
+export default Register;
