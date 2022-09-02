89b981d839ce967a442a86949f11d7b5ce17b60b
muhammadfadhil10 <muhammadfadhilakbar10@gmail.com>
2022-09-02 16:59:49 +0700

    add sort menu to folder pages

--- src/components/FolderList.js
+++ src/components/FolderList.js
@@ -1,4 +1,4 @@
-import { Container } from 'react-bootstrap';
+import { Container, Form } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Style from './FolderList.module.css';
@@ -6,6 +6,18 @@ import Style from './FolderList.module.css';
 const FolderList = () => {
 	return (
 		<>
+			<Form>
+				<Form.Group
+					className={`${Style.sortInput} d-flex justify-content-between`}
+				>
+					<Form.Label>Sort:</Form.Label>
+					<Form.Select size='sm'>
+						<option value='none'>None</option>
+						<option value='oldest'>Oldest</option>
+						<option value='newer'>newer</option>
+					</Form.Select>
+				</Form.Group>
+			</Form>
 			<Container
 				className={`${Style.container} d-flex flex-wrap justify-content-between`}
 			>
