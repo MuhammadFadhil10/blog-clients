cb4eda8ad6b8ad0c8ad38962338ba97e7c2baf9e
muhammadfadhil10 <muhammadfadhilakbar10@gmail.com>
2022-09-02 19:31:43 +0700

    finishing profile pages

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,10 +1,25 @@
-import { Container } from 'react-bootstrap';
+import { Container, Image } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+import Style from './Profile.module.css';
 
 const Profile = () => {
 	return (
 		<>
-			<Container>
-				<h1>Profile</h1>
+			<Container
+				className={`${Style.container} d-flex flex-column align-items-center`}
+			>
+				<Image className={`${Style.profilePicture}`}></Image>
+				<h5>userName</h5>
+				<h2>Name</h2>
+				<p className={`text-dark`}>
+					{' '}
+					bio Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe,
+					vitae?
+				</p>
+				<p>
+					<Link to='/profile'>https://lalala.com</Link>
+				</p>
 			</Container>
 		</>
 	);
