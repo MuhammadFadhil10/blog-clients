5602e9a740c04a57588228f8390b05d96964411d
muhammadfadhil10 <muhammadfadhilakbar10@gmail.com>
2022-09-02 16:48:36 +0700

    finishing folder component structure

--- src/components/FolderList.js
+++ src/components/FolderList.js
@@ -1,5 +1,33 @@
+import { Container } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Style from './FolderList.module.css';
+
 const FolderList = () => {
-	return;
+	return (
+		<>
+			<Container
+				className={`${Style.container} d-flex flex-wrap justify-content-between`}
+			>
+				<div className={`d-flex flex-column`}>
+					<div className={`${Style.folder} bg-dark`}></div>
+					<h3>folder one</h3>
+				</div>
+				<div className={`d-flex flex-column`}>
+					<div className={`${Style.folder} bg-dark`}></div>
+					<h3>folder one</h3>
+				</div>
+				<div className={`d-flex flex-column`}>
+					<div className={`${Style.folder} bg-dark`}></div>
+					<h3>folder one</h3>
+				</div>
+				<div className={`d-flex flex-column`}>
+					<div className={`${Style.folder} bg-dark`}></div>
+					<h3>folder one</h3>
+				</div>
+			</Container>
+		</>
+	);
 };
 
 export default FolderList;
--- src/pages/Folders.js
+++ src/pages/Folders.js
@@ -1,11 +1,13 @@
 import { Container } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import FolderList from '../components/FolderList';
 
 const Folders = () => {
 	return (
 		<>
 			<Container>
 				<h1>Your folders</h1>
+				<FolderList />
 			</Container>
 		</>
 	);
