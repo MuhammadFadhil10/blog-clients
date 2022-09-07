b71878a88e57f8c559c0be0ad9058d5d1b3e65c2
muhammadfadhil10 <muhammadfadhilakbar10@gmail.com>
2022-09-07 20:45:34 +0700

    finishing blog detail dummy structure

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -1,4 +1,5 @@
 import { Card, Button, Image, Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Class from './BlogList.module.css';
 
@@ -57,6 +58,35 @@ const BlogList = ({ type }) => {
 							</Card.Text>
 						</Card.Body>
 					</Card>
+				</Container>
+			)}
+			{type === 'related' && (
+				<Container className={`${Class.relatedBlogContainer} d-flex`}>
+					<Card className={`${Class.relatedBlogCard} d-flex flex-row`}>
+						<Card.Img variant='top' src='' />
+						<Card.Body
+							className={`${Class.relatedBlogCardBody} d-flex flex-column`}
+						>
+							<Card.Title>Card Title</Card.Title>
+							<Card.Text>
+								Some quick example text to build on the card title and make up
+								the bulk of the card's content.
+							</Card.Text>
+						</Card.Body>
+					</Card>
+					<Card className={`${Class.relatedBlogCard} d-flex flex-row`}>
+						<Card.Img variant='top' src='' />
+						<Card.Body
+							className={`${Class.relatedBlogCardBody} d-flex flex-column`}
+						>
+							<Card.Title>Card Title</Card.Title>
+							<Card.Text>
+								Some quick example text to build on the card title and make up
+								the bulk of the card's content.
+							</Card.Text>
+						</Card.Body>
+					</Card>
+					<Link to='/blog/asdsad'>{'See more >'}</Link>
 				</Container>
 			)}
 		</>
--- src/pages/BlogDetail.js
+++ src/pages/BlogDetail.js
@@ -1,12 +1,42 @@
-import { Container } from 'react-bootstrap';
+import { Container, Image } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Style from './BlogDetail.module.css';
+import BlogList from '../components/BlogList';
 
 const BlogDetail = () => {
 	return (
 		<Container>
-			<h1>Blog title</h1>
+			<div className={`${Style.blogHeader}`}>
+				<h1>Blog title</h1>
+				<h5 className='text-muted'>Author</h5>
+				<h5 className='text-muted'>created time</h5>
+				<Image src='' className={`${Style.blogThumbnail}`}></Image>
+			</div>
+			<div className={`${Style.blogBody}`}>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum
+					expedita voluptatibus ex repudiandae, cum necessitatibus voluptatem
+					velit veritatis eum magnam corporis cumque tempora itaque doloremque
+					reiciendis eaque perspiciatis unde tenetur corrupti ea impedit.
+					mollitia saepe velit ipsa voluptas aliquam quaerat est repudiandae,
+					quidem voluptate beatae similique odio placeat voluptatibus cupiditate
+					provident vero quae eaque pariatur minima id. <br></br>
+					<br></br> Labore provident asperiores similique veritatis totam,
+					laboriosam ex enim odit dolorum, mollitia itaque natus quisquam porro
+					nulla non veniam consequatur aliquam quo error. Adipisci dignissimos
+					iure enim nam unde quo a corrupti soluta veniam rem.
+				</p>
+			</div>
+			<div className={`${Style.blogBottom}`}>
+				<div className={`${Style.relatedBlogsContainer} d-flex flex-column`}>
+					<h1>Related Blogs</h1>
+					<BlogList type='related' />
+				</div>
+				<div className={`${Style.commentsContainer}`}>
+					<h1>Comments</h1>
+				</div>
+			</div>
 		</Container>
 	);
 };
