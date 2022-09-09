d5ad671e9c33b36826a1e7c5414e077f03c71fc8
muhammadfadhil10 <muhammadfadhilakbar10@gmail.com>
2022-09-09 17:21:19 +0700

    create custom hooks for fetching data with axios, with post,get,patch,delete method

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,12 +1,51 @@
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 
-const useFetch = ({ method, endpoint, data }) => {
-	method === 'post' &&
-		axios
-			.post(`${process.env.REACT_APP_API_ENDPOINT}/${endpoint}`, data)
-			.then((result) => {
-				console.log(result);
-			});
+const useFetch = (method, endpoint, data, trigger) => {
+	const [fetchResult, setFetchResult] = useState(null);
+	useEffect(
+		() => {
+			if (method === 'post') {
+				axios
+					.post(`${process.env.REACT_APP_API_ENDPOINT}${endpoint}`, data)
+					.then((result) => {
+						setFetchResult(result);
+					})
+					.catch((err) => {
+						setFetchResult(err);
+					});
+			} else if (method === 'get') {
+				axios
+					.get(`${process.env.REACT_APP_API_ENDPOINT}${endpoint}`)
+					.then((result) => {
+						setFetchResult(result);
+					})
+					.catch((err) => {
+						setFetchResult(err);
+					});
+			} else if (method === 'patch') {
+				axios
+					.patch(`${process.env.REACT_APP_API_ENDPOINT}${endpoint}`, data)
+					.then((result) => {
+						setFetchResult(result);
+					})
+					.catch((err) => {
+						setFetchResult(err);
+					});
+			} else if (method === 'delete') {
+				axios
+					.delete(`${process.env.REACT_APP_API_ENDPOINT}${endpoint}`)
+					.then((result) => {
+						setFetchResult(result);
+					})
+					.catch((err) => {
+						setFetchResult(err);
+					});
+			}
+		},
+		trigger ? [trigger] : []
+	);
+	return fetchResult;
 };
 
 export default useFetch;
